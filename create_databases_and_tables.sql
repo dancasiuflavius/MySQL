bad208a9ebcbc323a0b3c993b7c4159c6543c25e
Dancasiu Flavius <dancasiu.flavius@yahoo.com>
2023-12-01 23:05:27 +0200

    commit

--- create_databases_and_tables.sql
+++ create_databases_and_tables.sql
@@ -6,3 +6,15 @@ name VARCHAR(50),
 breed VARCHAR(50),
 age INT
 );
+
+
+CREATE TABLE employees
+(
+id int AUTO_INCREMENT PRIMARY KEY,
+last_name VARCHAR(50) NOT NULL,
+first_name VARCHAR(50) NOT NULL,
+middle_name VARCHAR(50),
+age INT NOT NULL,
+current_status VARCHAR(50) NOT NULL DEFAULT 'employed'
+);
+DESC employees;
